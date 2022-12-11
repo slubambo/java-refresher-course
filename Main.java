d140dfdc9d7ed62bcdb368455a2f9970143a08b5
slubambo <slubambo57@gmail.com>
2022-12-11 11:26:37 +0300

    Main Function and declaring multiple variables

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    //Declaring multiple variables
+    int x, y, z;
+    x = y = z = 50;
+    System.out.println(x + y + z);
+  }
+}
