986710bac51028f339a691f81c3fe3dc0aa9c92f
slubambo <slubambo57@gmail.com>
2022-12-12 08:35:21 +0300

    Recursion

---
+++ Recursion.java
@@ -0,0 +1,15 @@
+public class Recursion {
+
+  public static void main(String[] args) {
+    int result = sum(5, 10);
+    System.out.println(result);
+  }
+
+  public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end - 1);
+    } else {
+      return end;
+    }
+  }
+}
