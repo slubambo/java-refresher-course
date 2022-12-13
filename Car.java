9a90e70452a530f09573be57cdcb6fd1256f4b5b
slubambo <slubambo57@gmail.com>
2022-12-13 14:51:44 +0300

    Car Class

---
+++ Car.java
@@ -0,0 +1,19 @@
+public class Car {
+
+  // Create a fullThrottle() method
+  public void fullThrottle() {
+    System.out.println("The car is going as fast as it can!");
+  }
+
+  // Create a speed() method and add a parameter
+  public void speed(int maxSpeed) {
+    System.out.println("Max speed is: " + maxSpeed);
+  }
+
+  // Inside main, call the methods on the myCar object
+  public static void main(String[] args) {
+    Car myCar = new Car(); // Create a myCar object
+    myCar.fullThrottle(); // Call the fullThrottle() method
+    myCar.speed(200); // Call the speed() method
+  }
+}
