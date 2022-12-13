e32382a5181e92f83d663b82959b5a2abc9851ed
slubambo <slubambo57@gmail.com>
2022-12-13 14:47:22 +0300

    Methods

--- OOPBasics.java
+++ OOPBasics.java
@@ -2,8 +2,27 @@ public class OOPBasics {
 
   int x = 5;
 
+  // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
   public static void main(String[] args) {
     OOPBasics myObj = new OOPBasics();
+
+    myObj.x = 25; //Modify attributes
+
     System.out.println(myObj.x);
+
+    myStaticMethod(); // Call the static method
+    // myPublicMethod(); This would compile an error
+
+    OOPBasics myObjt = new OOPBasics(); // Create an object of Main
+    myObjt.myPublicMethod(); // Call the public method on the object
   }
 }
