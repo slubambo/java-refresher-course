578173185c13386e8e06f3c03d839eaaf0b608a9
slubambo <slubambo57@gmail.com>
2022-12-14 10:12:42 +0300

    Inner Classes

---
+++ OuterInnerClass.java
@@ -0,0 +1,18 @@
+class OuterClass {
+
+  int x = 10;
+
+  class InnerClass {
+
+    int y = 5;
+  }
+}
+
+public class OuterInnerClass {
+
+  public static void main(String[] args) {
+    OuterClass myOuter = new OuterClass();
+    OuterClass.InnerClass myInner = myOuter.new InnerClass();
+    System.out.println(myInner.y + myOuter.x);
+  }
+}
